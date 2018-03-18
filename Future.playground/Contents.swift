31a6f69481be19301dd1ac2d60f1cf4b65c4cdef
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-18 14:00:04 +0100

    Escaping the Future!

--- Future.playground/Contents.swift
+++ Future.playground/Contents.swift
@@ -6,15 +6,17 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 struct Future<A> {
-    let task: ((A) -> ()) -> ()
+    let task: (@escaping (A) -> ()) -> ()
 }
 
 let getNumber = {
     return 23 + 26
 }
 
-let task: ((Int) -> ()) -> () = { continuation in
-    continuation(getNumber())
+let task: (@escaping (Int) -> ()) -> () = { continuation in
+    DispatchQueue.global().async {
+        continuation(getNumber())
+    }
 }
 
 Future(task: task)
