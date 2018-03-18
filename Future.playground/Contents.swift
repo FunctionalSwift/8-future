29563ad1ced63a02da2258cbca1c0445d301b7cd
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-18 11:18:03 +0100

    Using GCD... Oh, no!!! We have lost the ability to return user data!! :(

--- Future.playground/Contents.swift
+++ Future.playground/Contents.swift
@@ -1,14 +1,14 @@
 //: Playground - Future
 
-func createUser(name: String, password: String, premium: Bool, newsletter: Bool) -> Result<User, UserError> {
-    return curry(User.init)
-        <%> Validators.Name(name)
-        <*> Validators.Password(password)
-        <*> Result.pure(newsletter)
-        <*> Result.pure(premium)
-        >>= (UserValidator.Premium || UserValidator.Newsletter)
+import Foundation
+
+func createUser(name: String, password: String, premium: Bool, newsletter: Bool) {
+    DispatchQueue.global().async {
+        curry(User.init)
+            <%> Validators.Name(name)
+            <*> Validators.Password(password)
+            <*> Result.pure(newsletter)
+            <*> Result.pure(premium)
+            >>= (UserValidator.Premium || UserValidator.Newsletter)
+    }
 }
-
-let user = createUser(name: "alex", password: "functionalswift", premium: true, newsletter: false)
-
-user.map { print("SUCCESS: User created - \($0)") }
