f95ada2625f5b0472661e0a52f18e5609ab5b205
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-18 14:14:25 +0100

    Future blog example, Are you ready?

--- Future.playground/Contents.swift
+++ Future.playground/Contents.swift
@@ -48,8 +48,54 @@ struct Future<A> {
     }
 }
 
-Future.async({ 23 + 19 })
-    .map { $0 + 3 }
-    .runAsync { number in
-        print(number)
+func parse(from url: String) -> [String: AnyObject] {
+    return URL(string: url)
+        .flatMap { try! Data(contentsOf: $0) }
+        .flatMap { try! JSONSerialization.jsonObject(with: $0) }
+        .flatMap { $0 as? [String: AnyObject] }!
 }
+
+struct Post {
+    let title: String
+    let content: String
+    let authorId: Int
+    
+    static func decode(_ json: [String: AnyObject]) -> Post {
+        let title = json["title"] as! String
+        let content = json["content"] as! String
+        let authorId = json["userId"] as! Int
+        
+        return Post(title: title, content: content, authorId: authorId)
+    }
+    
+    static func get(_ id: Int) -> Future<Post> {
+        return Future.async {
+            let json = parse(from: "http://functionalhub.com/exercises/posts/\(id)"
+            )
+            
+            return Post.decode(json)
+        }
+    }
+}
+
+struct Author {
+    let firstname: String
+    let lastName: String
+    
+    static func decode(_ json: [String: AnyObject]) -> Author {
+        let firstName = json["firstName"] as! String
+        let lastName = json["lastName"] as! String
+        
+        return Author(firstname: firstName, lastName: lastName)
+    }
+    
+    static func get(_ id: Int) -> Future<Author> {
+        return Future.async {
+            let json = parse(from: "http://functionalhub.com/exercises/users/\(id)"
+            )
+            
+            return Author.decode(json)
+        }
+    }
+}
+
