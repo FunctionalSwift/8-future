8e5905b989ce194dd1a32830553966cbf8fb6c40
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-18 11:32:58 +0100

    Our validators are again synchronous but it is not our best solution. We're still working on it!!

--- Future.playground/Contents.swift
+++ Future.playground/Contents.swift
@@ -5,19 +5,31 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-func createUser(name: String, password: String, premium: Bool, newsletter: Bool, _ completion: @escaping (Result<User, UserError>) -> Void) {
-    DispatchQueue.global().async {
-        let result = curry(User.init)
-            <%> Validators.Name(name)
-            <*> Validators.Password(password)
-            <*> Result.pure(newsletter)
-            <*> Result.pure(premium)
-            >>= (UserValidator.Premium || UserValidator.Newsletter)
-        
-        completion(result)
+func createUser(name: String, password: String, premium: Bool, newsletter: Bool) -> Result<User, UserError> {
+    
+    let group = DispatchGroup()
+    
+    var nameResult: Result<String, UserError>?
+    var passwordResult: Result<String, UserError>?
+    
+    DispatchQueue.global().async(group: group) {
+        nameResult = Validators.Name(name)
     }
+    
+    DispatchQueue.global().async(group: group) {
+        passwordResult = Validators.Password(password)
+    }
+    
+    group.wait()
+    
+    return curry(User.init)
+        <%> nameResult!
+        <*> passwordResult!
+        <*> Result.pure(newsletter)
+        <*> Result.pure(premium)
+        >>= (UserValidator.Premium || UserValidator.Newsletter)
 }
 
-createUser(name: "alex", password: "functionalSwift", premium: true, newsletter: true) { result in
-    result.map { print($0) }
-}
+
+createUser(name: "alex", password: "functionalSwift", premium: true, newsletter: true)
+    .map { print($0) }
