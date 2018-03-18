c99315a8f7f371bfaf6319d8437f7b941cb74ec8
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-18 11:21:57 +0100

    Do you remember our friend CPS? He'll help us to solve that!!

--- Future.playground/Contents.swift
+++ Future.playground/Contents.swift
@@ -1,14 +1,23 @@
 //: Playground - Future
 
 import Foundation
+import PlaygroundSupport
 
-func createUser(name: String, password: String, premium: Bool, newsletter: Bool) {
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+func createUser(name: String, password: String, premium: Bool, newsletter: Bool, _ completion: @escaping (Result<User, UserError>) -> Void) {
     DispatchQueue.global().async {
-        curry(User.init)
+        let result = curry(User.init)
             <%> Validators.Name(name)
             <*> Validators.Password(password)
             <*> Result.pure(newsletter)
             <*> Result.pure(premium)
             >>= (UserValidator.Premium || UserValidator.Newsletter)
+        
+        completion(result)
     }
+}
+
+createUser(name: "alex", password: "functionalSwift", premium: true, newsletter: true) { result in
+    result.map { print($0) }
 }
