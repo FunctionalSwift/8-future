3c0c52c60bbb9f41579bd26924ff5734b0c10dde
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-18 13:56:59 +0100

    The Future is now!!

--- Future.playground/Contents.swift
+++ Future.playground/Contents.swift
@@ -5,31 +5,12 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-func createUser(name: String, password: String, premium: Bool, newsletter: Bool) -> Result<User, UserError> {
-    
-    let group = DispatchGroup()
-    
-    var nameResult: Result<String, UserError>?
-    var passwordResult: Result<String, UserError>?
-    
-    DispatchQueue.global().async(group: group) {
-        nameResult = Validators.Name(name)
-    }
-    
-    DispatchQueue.global().async(group: group) {
-        passwordResult = Validators.Password(password)
-    }
-    
-    group.wait()
-    
-    return curry(User.init)
-        <%> nameResult!
-        <*> passwordResult!
-        <*> Result.pure(newsletter)
-        <*> Result.pure(premium)
-        >>= (UserValidator.Premium || UserValidator.Newsletter)
+struct Future {
+    let task: () -> ()
 }
 
+let task: () -> () = {
+    
+}
 
-createUser(name: "alex", password: "functionalSwift", premium: true, newsletter: true)
-    .map { print($0) }
+Future(task: task)
